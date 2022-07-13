ff7fbe6afa3f683ca3ae0e5f4bf8cc4820656204
Eng:Saeed-Ramadan <83456924+Saeed-Ramadan@users.noreply.github.com>
2022-07-13 11:29:28 +0200

    SR Notes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { nanoid } from "nanoid";
+import { useEffect, useState } from "react";
+import NotesList from "./components/NotesList";
+import Search from "./components/Search";
+import Header from "./components/Header";
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([{
+    id: nanoid(),
+    text: "Welcome",
+    date:'10/7/2022'
+    },]);
+
+  /**to search about the note */
+  const [searchText, setSearchText] = useState("");
+
+  /**to dark mode */
+  const [darkMode, setDarkMode] = useState(false);
+
+  /**to get data from the local storage */
+  useEffect(()=> {
+    const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data") );
+
+    if (savedNotes){
+      setNotes(savedNotes)
+    } else {
+      /**to save data in the local storage */
+      localStorage.setItem("react-notes-app-data", JSON.stringify(notes))
+    }
+  }, []);
+
+  /*to add note to the ui */
+  const addNote = (text) => {
+    //console.log(text);
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString() /*to replace the date to our country date format */,
+    };
+    const newNotes = [
+      ...notes,
+      newNote,
+    ]; /*to add the new note to array of notes */
+    setNotes(newNotes); /**to rerender the new data to the ui */
+    /** save the new notes after modify it. */
+    localStorage.setItem("react-notes-app-data", JSON.stringify(newNotes));
+  };
+
+  /**to delete the note */
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+    /** save the new notes after modify it. */
+    localStorage.setItem("react-notes-app-data", JSON.stringify(newNotes));
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={`${darkMode && "dark-mode"}`} /**to display the dark mode   */
+    >
+      <div className="container">
+        <Header  handleToggleDarkMode={setDarkMode} />
+        <Search handleSearchNote={setSearchText} />
+        <NotesList
+          notes={notes.filter((note) =>
+            note.text.toLowerCase().includes(searchText)
+          )}
+          /** to filter the words in the noy to search */ handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/AddNode.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const AddNote = ({ handleAddNote }) => {
+    const[noteText , setNoteText] = useState('');
+    const characterLimit = 500;
+
+    const handleChange = (event) => {
+        if (characterLimit - event.target.value.length >= 0){
+            setNoteText(event.target.value);
+        }
+    }
+
+    const handleSaveClick = () => {
+        if(noteText.trim().length > 0){
+            handleAddNote(noteText);
+            setNoteText('');
+        } /**to check if there a character in the note or not if there is not < don not save < and back the new empty note empty */
+    }
+    return (
+        <div className="note new">
+            <textarea
+                rows='8'
+                cols='10'
+                placeholder="Type to add add a note..."
+                value={noteText}
+                onChange={handleChange}
+            ></textarea>
+            <div className="note-footer">
+                <small>{characterLimit - noteText.length} Remaing</small>
+                <button className="save" onClick={handleSaveClick}>Save</button>
+            </div>
+        </div>
+    )
+}
+
+export default AddNote;
